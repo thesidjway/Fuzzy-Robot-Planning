ca1aa6c59d4080ab503499bf1d6d191173f8126c
Siddharth Jha <sidchicharito@gmail.com>
2017-03-26 16:26:01 +0530

    Skeleton Code Completed

--- Projectcode/Projectcode.ino
+++ Projectcode/Projectcode.ino
@@ -20,6 +20,7 @@ int robotAngle = 0; // current Angle of the robot
 
 int getRobotAngle()
 {
+  //ADD ANGLE DIFFERENCE CALCULATION CODE HERE
   return robotAngle;
 }
 
@@ -27,7 +28,7 @@ int getRobotAngle()
 void getAllDistances()
 {
   int temp_dist1, temp_dist2, temp_dist3, temp_dist4; // temporary variables for storing data, so that all distances get written simultaneously.
-  /*Insert SONAR data collection code here*/
+  /*Insert distance data collection code here*/
   dist1 = temp_dist1;
   dist2 = temp_dist2;
   dist3 = temp_dist3;
@@ -36,14 +37,16 @@ void getAllDistances()
 
 void updateOdometry()
 {
-
+  //ADD ODOMETRY CODE HERE
 }
 
 
 // Step 1 -  Instantiating an object library
 Fuzzy* fuzzy = new Fuzzy();
 
-void setup() {
+void setup() 
+{
+  
   Serial.begin(9600);
 
   pinMode(LM1, OUTPUT); // left motor input1
@@ -94,7 +97,7 @@ void setup() {
   FuzzyOutput* vr = new FuzzyOutput(2);
 
   // Creating FuzzySet to compond FuzzyOutput vl (Left Wheel Velocity)
-  FuzzySet* still = new FuzzySet(0, 0, 0, 0);
+  FuzzySet* veryslow = new FuzzySet(0, 0, 0, 0);
   FuzzySet* slow = new FuzzySet(100, 130, 130, 160);
   FuzzySet* mid = new FuzzySet(130, 160, 160, 190);
   FuzzySet* fast = new FuzzySet(160, 190, 190, 220);
@@ -102,22 +105,22 @@ void setup() {
 
   // Creating FuzzySet to compond FuzzyOutput vr (Right Wheel Velocity)
 
-  vl->addFuzzySet(still);
+  vl->addFuzzySet(veryslow);
   vl->addFuzzySet(slow);
   vl->addFuzzySet(mid);
   vl->addFuzzySet(fast);
   vl->addFuzzySet(veryfast);
 
 
-  vr->addFuzzySet(still);
+  vr->addFuzzySet(veryslow);
   vr->addFuzzySet(slow);
   vr->addFuzzySet(mid);
   vr->addFuzzySet(fast);
   vr->addFuzzySet(veryfast);
 
 
-  fuzzy->addFuzzyOutput(vl); // Add FuzzyOutput to Fuzzy object
-  fuzzy->addFuzzyOutput(vr); // Add FuzzyOutput to Fuzzy object
+  fuzzy->addFuzzyOutput(vl); 
+  fuzzy->addFuzzyOutput(vr); 
 
 
   FuzzyRuleAntecedent*  VSNB = new FuzzyRuleAntecedent();
@@ -198,55 +201,143 @@ void setup() {
   VBPB->joinWithAND(verybig, posbig);
 
 
+  FuzzyRuleConsequent* VFVS = new FuzzyRuleConsequent();
+  VFVS->addOutput(veryfast);
+  VFVS->addOutput(veryslow);
+  FuzzyRuleConsequent* FVS = new FuzzyRuleConsequent();
+  FVS->addOutput(fast);
+  FVS->addOutput(veryslow);
+  FuzzyRuleConsequent* MVS = new FuzzyRuleConsequent();
+  MVS->addOutput(mid);
+  MVS->addOutput(veryslow);
+  FuzzyRuleConsequent* SVS = new FuzzyRuleConsequent();
+  SVS->addOutput(slow);
+  SVS->addOutput(veryslow);
+
+  FuzzyRuleConsequent* SS = new FuzzyRuleConsequent();
+  SS->addOutput(slow);
+  SS->addOutput(slow);
+  FuzzyRuleConsequent* MM = new FuzzyRuleConsequent();
+  MM->addOutput(mid);
+  MM->addOutput(mid);
+  FuzzyRuleConsequent* FF = new FuzzyRuleConsequent();
+  FF->addOutput(fast);
+  FF->addOutput(fast);
+  FuzzyRuleConsequent* VFVF = new FuzzyRuleConsequent();
+  VFVF->addOutput(veryfast);
+  VFVF->addOutput(veryfast);
+
+
+  FuzzyRuleConsequent* VSVF = new FuzzyRuleConsequent();
+  VSVF->addOutput(veryslow);
+  VSVF->addOutput(veryfast);
+  FuzzyRuleConsequent* VSF = new FuzzyRuleConsequent();
+  VSF->addOutput(veryslow);
+  VSF->addOutput(fast);
+  FuzzyRuleConsequent* VSM = new FuzzyRuleConsequent();
+  VSM->addOutput(veryslow);
+  VSM->addOutput(mid);
+  FuzzyRuleConsequent* VSS = new FuzzyRuleConsequent();
+  VSS->addOutput(veryslow);
+  VSS->addOutput(slow);
+
+  FuzzyRule* fuzzyRule01 = new FuzzyRule(1,  VSNB, FVS   );
+  FuzzyRule* fuzzyRule02 = new FuzzyRule(2,  SNB,  VFVS  );
+  FuzzyRule* fuzzyRule03 = new FuzzyRule(3,  MNB,  VFVS  );
+  FuzzyRule* fuzzyRule04 = new FuzzyRule(4,  BNB,  VFVS  );
+  FuzzyRule* fuzzyRule05 = new FuzzyRule(5,  VBNB, VFVS  );
+  FuzzyRule* fuzzyRule06 = new FuzzyRule(6,  VSNM, MVS   );
+  FuzzyRule* fuzzyRule07 = new FuzzyRule(7,  SNM,  FVS   );
+  FuzzyRule* fuzzyRule08 = new FuzzyRule(8,  MNM,  VFVS  );
+  FuzzyRule* fuzzyRule09 = new FuzzyRule(9,  BNM,  VFVS  );
+  FuzzyRule* fuzzyRule10 = new FuzzyRule(10, VBNM, VFVS  );
+  FuzzyRule* fuzzyRule11 = new FuzzyRule(11, VSNS, SVS   );
+  FuzzyRule* fuzzyRule12 = new FuzzyRule(12, SNS,  MVS   );
+  FuzzyRule* fuzzyRule13 = new FuzzyRule(13, MNS,  FVS   );
+  FuzzyRule* fuzzyRule14 = new FuzzyRule(14, BNS,  VFVS  );
+  FuzzyRule* fuzzyRule15 = new FuzzyRule(15, VBNS, VFVS  );
+  FuzzyRule* fuzzyRule16 = new FuzzyRule(16, VSZ,  SS    );
+  FuzzyRule* fuzzyRule17 = new FuzzyRule(17, SZ,   SS    );
+  FuzzyRule* fuzzyRule18 = new FuzzyRule(18, MZ,   MM    );
+  FuzzyRule* fuzzyRule19 = new FuzzyRule(19, BZ,   FF    );
+  FuzzyRule* fuzzyRule20 = new FuzzyRule(20, VBZ,  VFVF  );
+  FuzzyRule* fuzzyRule21 = new FuzzyRule(21, VSPS, VSS   );
+  FuzzyRule* fuzzyRule22 = new FuzzyRule(22, SPS,  VSM   );
+  FuzzyRule* fuzzyRule23 = new FuzzyRule(23, MPS,  VSF   );
+  FuzzyRule* fuzzyRule24 = new FuzzyRule(24, BPS,  VSVF  );
+  FuzzyRule* fuzzyRule25 = new FuzzyRule(25, VBPS, VSVF  );
+  FuzzyRule* fuzzyRule26 = new FuzzyRule(26, VSPM, VSS   );
+  FuzzyRule* fuzzyRule27 = new FuzzyRule(27, SPM,  VSF   );
+  FuzzyRule* fuzzyRule28 = new FuzzyRule(28, MPM,  VSVF  );
+  FuzzyRule* fuzzyRule29 = new FuzzyRule(29, BPM,  VSVF  );
+  FuzzyRule* fuzzyRule30 = new FuzzyRule(30, VBPM, VSVF  );
+  FuzzyRule* fuzzyRule31 = new FuzzyRule(31, VSPB, VSM   );
+  FuzzyRule* fuzzyRule32 = new FuzzyRule(32, SPB,  VSVF  );
+  FuzzyRule* fuzzyRule33 = new FuzzyRule(33, MPB,  VSVF  );
+  FuzzyRule* fuzzyRule34 = new FuzzyRule(34, BPB,  VSVF  );
+  FuzzyRule* fuzzyRule35 = new FuzzyRule(35, VBPB, VSVF  );
+
+  fuzzy->addFuzzyRule(fuzzyRule01);
+  fuzzy->addFuzzyRule(fuzzyRule02);
+  fuzzy->addFuzzyRule(fuzzyRule03);
+  fuzzy->addFuzzyRule(fuzzyRule04);
+  fuzzy->addFuzzyRule(fuzzyRule05);
+  fuzzy->addFuzzyRule(fuzzyRule06);
+  fuzzy->addFuzzyRule(fuzzyRule07);
+  fuzzy->addFuzzyRule(fuzzyRule08);
+  fuzzy->addFuzzyRule(fuzzyRule09);
+  fuzzy->addFuzzyRule(fuzzyRule10);
+  fuzzy->addFuzzyRule(fuzzyRule11);
+  fuzzy->addFuzzyRule(fuzzyRule12);
+  fuzzy->addFuzzyRule(fuzzyRule13);
+  fuzzy->addFuzzyRule(fuzzyRule14);
+  fuzzy->addFuzzyRule(fuzzyRule15);
+  fuzzy->addFuzzyRule(fuzzyRule16);
+  fuzzy->addFuzzyRule(fuzzyRule17);
+  fuzzy->addFuzzyRule(fuzzyRule18);
+  fuzzy->addFuzzyRule(fuzzyRule19);
+  fuzzy->addFuzzyRule(fuzzyRule20);
+  fuzzy->addFuzzyRule(fuzzyRule21);
+  fuzzy->addFuzzyRule(fuzzyRule22);
+  fuzzy->addFuzzyRule(fuzzyRule23);
+  fuzzy->addFuzzyRule(fuzzyRule24);
+  fuzzy->addFuzzyRule(fuzzyRule25);
+  fuzzy->addFuzzyRule(fuzzyRule26);
+  fuzzy->addFuzzyRule(fuzzyRule27);
+  fuzzy->addFuzzyRule(fuzzyRule28);
+  fuzzy->addFuzzyRule(fuzzyRule29);
+  fuzzy->addFuzzyRule(fuzzyRule30);
+  fuzzy->addFuzzyRule(fuzzyRule31);
+  fuzzy->addFuzzyRule(fuzzyRule32);
+  fuzzy->addFuzzyRule(fuzzyRule33);
+  fuzzy->addFuzzyRule(fuzzyRule34);
+  fuzzy->addFuzzyRule(fuzzyRule35);
 
 
 
-  /*
-    //Passo 4 - Assembly the Fuzzy rules
-    // FuzzyRule "IF distance = samll THEN velocity = slow"
-    FuzzyRuleAntecedent* ifDistanceSmall = new FuzzyRuleAntecedent(); // Instantiating an Antecedent to expression
-    ifDistanceSmall->joinSingle(small); // Adding corresponding FuzzySet to Antecedent object
-    FuzzyRuleConsequent* thenVelocitySlow = new FuzzyRuleConsequent(); // Instantiating a Consequent to expression
-    thenVelocitySlow->addOutput(slow);// Adding corresponding FuzzySet to Consequent object
-    // Instantiating a FuzzyRule object
-    FuzzyRule* fuzzyRule01 = new FuzzyRule(1, ifDistanceSmall, thenVelocitySlow); // Passing the Antecedent and the Consequent of expression
 
-    fuzzy->addFuzzyRule(fuzzyRule01); // Adding FuzzyRule to Fuzzy object
-
-    // FuzzyRule "IF distance = safe THEN velocity = normal"
-    FuzzyRuleAntecedent* ifDistanceSafe = new FuzzyRuleAntecedent(); // Instantiating an Antecedent to expression
-    ifDistanceSafe->joinSingle(safe); // Adding corresponding FuzzySet to Antecedent object
-    FuzzyRuleConsequent* thenVelocityAverage = new FuzzyRuleConsequent(); // Instantiating a Consequent to expression
-    thenVelocityAverage->addOutput(average); // Adding corresponding FuzzySet to Consequent object
-    // Instantiating a FuzzyRule object
-    FuzzyRule* fuzzyRule02 = new FuzzyRule(2, ifDistanceSafe, thenVelocityAverage); // Passing the Antecedent and the Consequent of expression
-
-    fuzzy->addFuzzyRule(fuzzyRule02); // Adding FuzzyRule to Fuzzy object
-
-    // FuzzyRule "IF distance = big THEN velocity = fast"
-    FuzzyRuleAntecedent* ifDistanceBig = new FuzzyRuleAntecedent(); // Instantiating an Antecedent to expression
-    ifDistanceBig->joinSingle(big); // Adding corresponding FuzzySet to Antecedent object
-    FuzzyRuleConsequent* thenVelocityFast = new FuzzyRuleConsequent(); // Instantiating a Consequent to expression
-    thenVelocityFast->addOutput(fast);// Adding corresponding FuzzySet to Consequent object
-    // Instantiating a FuzzyRule object
-    FuzzyRule* fuzzyRule03 = new FuzzyRule(3, ifDistanceBig, thenVelocityFast); // Passing the Antecedent and the Consequent of expression
-
-    fuzzy->addFuzzyRule(fuzzyRule03); // Adding FuzzyRule to Fuzzy object
-  */
 }
 
-void loop() {
+void loop() 
+{
   getAllDistances();
-
-  // Step 5 - Report inputs value, passing its ID and value
-  // fuzzy->setInput(1, dist);
-  // Step 6 - Exe the fuzzification
+  getRobotAngle();
+  updateOdometry();
+  
+  
+  
+  fuzzy->setInput(1, 10); //todo: ADD DISTANCE FROM TARGET
+  fuzzy->setInput(2, 20); //todo: ADD ANGLE DIFFERENCE
+  
   fuzzy->fuzzify();
-  // Step 7 - Exe the desfuzzyficação for each output, passing its ID
-  float output = fuzzy->defuzzify(1);
 
-  Serial.println(output);
-  //setRobotSpeed(output);
+  float targetvl = fuzzy->defuzzify(1);
+  float targetvr = fuzzy->defuzzify(2);
+
+  Serial.print("Vl: \t");
+  Serial.print(targetvl);
+  Serial.print("\t Vr: \t");
+  Serial.println(targetvr);
 
   delay(100);
 }
